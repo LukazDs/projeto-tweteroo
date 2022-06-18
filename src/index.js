127c2571f3b6c5565f5138ffb0ce54761cb363e6
Lucas Duarte <lukazds2016@gmail.com>
2022-06-18 15:43:04 -0300

    Impletando lógica de exibição dos dez últimos tweets

--- src/index.js
+++ src/index.js
@@ -5,97 +5,44 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-const users = [
-    {
-	    username: 'bobesponja', 
-	    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-    },
-    {
-	    username: 'bobesponja', 
-	    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-    },
-    {
-	    username: 'bobesponja', 
-	    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-    },
-    {
-	    username: 'bobesponja', 
-	    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-    }
-];
-const tweets = [
-    {
-	    username: "bobesponja1",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja2",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja3",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja4",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja5",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja6",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja7",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja8",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja9",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja10",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja11",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja12",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja13",
-        tweet: "eu amo o hub",
-    },
-    {
-	    username: "bobesponja14",
-        tweet: "eu amo o hub",
-    }
-];
+const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
     const user = req.body;
     users.push(user);
+
     res.send("OK");
 })
 
 app.get("/tweets", (req, res) => {
-    const mainTweets = tweets.reverse().slice(0, 10)
-    res.send(mainTweets.reverse());
+
+    let initial = 0;
+    let end = 0;
+
+    if (tweets.length <= 10) {
+        initial = 0;
+        end = tweets.length;
+    }
+    else {
+        initial = tweets.length - 10;
+        end = tweets.length;
+    }
+
+    const lastTweets = tweets.slice(initial, end);
+    const lastUsers = users.slice(initial, end);
+
+    lastTweets.map((v, i) => v.avatar = lastUsers[i].avatar);
+
+    res.send(lastTweets.reverse());
 })
 
 app.post("/tweets", (req, res) => {
     const tweet = req.body;
     tweets.push(tweet);
+    if (tweets.length > 1) {
+        users.push(users[users.length - 1])
+    }
     res.send("OK");
 })
 
