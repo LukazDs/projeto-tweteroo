4ac52989ab2898efb0091e02c8fef299fce0cd99
Lucas Duarte <lukazds2016@gmail.com>
2022-06-18 11:51:24 -0300

    Definindo variáveis globais

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json())
 
-app.get("/ola", (req, res) => {
+const users = [];
+const tweets = [];
+const user = 
+    {
+	    username: 'bobesponja', 
+	    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+    };
+
+app.get("/sign-up", (req, res) => {
     res.send("TESTE: OLÁ")
 })
 
